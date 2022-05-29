7fb18fe11384a63905269362dff2fb00ed5f03bc
mcaffandi <m.c.affandi@gmail.com>
2022-05-29 23:54:42 +0700

    index.js

--- index.js
+++ index.js
@@ -23,20 +23,39 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+
+  let hasilLooping = []
+  for (let index = 0; index < arrPegawai.length; index++) {
+    hasilLooping.push(arrPegawai[index].namaDepan + " " + arrPegawai[index].namaBelakang);
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+
+  let jumlahPria = 0;
+  const jumlahPria1 = []
+  for (let index1 = 0; index1 < arrPegawai.length; index1++) {
+    if (arrPegawai[index1].jenisKelamin === 'M') {
+      jumlahPria1.push(arrPegawai[index1].jenisKelamin);
+      jumlahPria = jumlahPria1.length
+    }
+
+  }
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
-
+  let jumlahWanita = 0;
+  const jumlahWanita1 = []
+  for (let index2 = 0; index2 < arrPegawai.length; index2++) {
+    if (arrPegawai[index2].jenisKelamin === 'F') {
+      jumlahWanita1.push(arrPegawai[index2].jenisKelamin);
+      jumlahWanita = jumlahWanita1.length
+    }
+  }
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
@@ -49,6 +68,16 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita"
+  }
+  else if (jumlahPria < jumlahWanita) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria"
+  }
+  else {
+    komentar = "Jumlah Pria dan Wanita berimbang"
+  }
+
 
   // ! JANGAN DIMODIFIKASI
   return {
@@ -65,6 +94,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
